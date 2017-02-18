0efe9e8efb6c1668c351ed139f4605dfe9d86cae
piees <yngveranestad@gmail.com>
2017-02-18 01:38:12 +0100

    add cors

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from getweather import getWeather
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
